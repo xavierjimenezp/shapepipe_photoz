16fb5c8d20567cc66f085ecca6e5b9c736566efc
xavierjimenezp <xavierjimenezp@gmail.com>
2021-02-16 21:56:05 +0100

    added params file

---
+++ params_ps3pi_cfis.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 12 10:02:58 2020
+
+@author: Xavier Jimenez
+"""
+
+#------------------------------------------------------------------#
+# # # # # PATH # # # # #
+#------------------------------------------------------------------#
+
+import os
+path = os.getcwd() + '/'
+
+#------------------------------------------------------------------#
+# # # # # GLOBAL PARAMS # # # # #
+#------------------------------------------------------------------#
+
+bands = ['R']
+output_path = path
+output_name = 'CFIS_matched_deep_2_3_catalog_R'
+temp_path = '/n17data/jimenez/temp/'
+
+#------------------------------------------------------------------#
+# # # # # PREPROCESSING PARAMS # # # # #
+#------------------------------------------------------------------#
+
+spectral_path = '/home/jimenez/spectral_catalogs/'
+spectral_names = ['alldeep.egs.uniq.2012jun13']
+path_to_tile_run = '/n17data/jimenez/shaperun/'
+spectral_surveys = ['deep23']
+vignet = False
+
+#------------------------------------------------------------------#
+# # # # # MACHINE LEARNING PARAMS # # # # #
+#------------------------------------------------------------------#
+
+max_evals = 200
+path_to_csv = path + 'catalogs/' + 'MediumDeep_CFHT_CFIS_R_matched_catalog_2' + '.csv'
+
+
+
